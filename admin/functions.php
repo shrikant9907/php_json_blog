855c3d7eecad403000e634d1ad02884e81180fd9
Shrikant Yadav <shrikant9907@gmail.com>
2021-08-13 22:58:46 +0530

    fixes:

--- admin/functions.php
+++ admin/functions.php
@@ -4,10 +4,10 @@
  
 //Remove Error Reporting
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-// error_reporting(E_ERROR | E_PARSE);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+error_reporting(E_ERROR | E_PARSE);
 
 /*  
  * Session Start;
